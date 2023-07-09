00a7f6f6a0c22375e108bfa614691bddc6657988
denikeev <script.edf@gmail.com>
2023-07-09 21:28:01 +0300

    add data when scrolling

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,53 @@
   import { onMounted } from 'vue';
   import { useCarsStore } from '@/stores/cars';
   import Car from './components/Car.vue';
+  import throttle from 'lodash/throttle.js';
   
-  const { carsData, fetchCars, isCarsLoaded } = useCarsStore();
-
+  const { carsData, fetchCars, isCarsLoaded, isCarsPartLoaded } = useCarsStore();
   onMounted(async () => {
     await fetchCars();
+
+    window.onscroll = throttle(function() {
+        if ((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) {
+          fetchCars();
+        }
+    }, 3000);
   });
+
 </script>
 
 <template>
-  <div class="table">
-    <div class="table__row table__row--head">
-      <div class="table__cell table__cell--head">Название</div>
-      <div class="table__cell table__cell--head">Расход топлива мл/г</div>
-      <div class="table__cell table__cell--head">Цилиндры</div>
-      <div class="table__cell table__cell--head">Рабочий объём</div>
-      <div class="table__cell table__cell--head">Л.C</div>
-      <div class="table__cell table__cell--head">Вес</div>
-      <div class="table__cell table__cell--head">Ускорение м/c2</div>
-      <div class="table__cell table__cell--head">Год выпуска</div>
-      <div class="table__cell table__cell--head">Производство</div>
+  <div>
+    <div class="table">
+      <div class="table__row table__row--head">
+        <div class="table__cell table__cell--head">Название</div>
+        <div class="table__cell table__cell--head">Расход топлива мл/г</div>
+        <div class="table__cell table__cell--head">Цилиндры</div>
+        <div class="table__cell table__cell--head">Рабочий объём</div>
+        <div class="table__cell table__cell--head">Л.C</div>
+        <div class="table__cell table__cell--head">Вес</div>
+        <div class="table__cell table__cell--head">Ускорение м/c2</div>
+        <div class="table__cell table__cell--head">Год выпуска</div>
+        <div class="table__cell table__cell--head">Производство</div>
+      </div>
+      <template v-if="isCarsLoaded">
+        <Car 
+          v-for="car in carsData"
+          :key="car.id"
+          v-bind:car="car"
+          v-bind:key="car.id"
+        ></Car>
+      </template>
+      <img v-show="!isCarsLoaded || !isCarsPartLoaded" class="preloader" src="./preloader.gif" width="64" height="64" alt="Загрузка..." />
     </div>
-    <template v-if="isCarsLoaded">
-      <Car 
-        v-for="car in carsData"
-        :key="car.id"
-        v-bind:car="car"
-        v-bind:key="car.id"
-      ></Car>
-    </template>
-    <img v-else src="./preloader.gif" width="64" height="64" alt="Загрузка..." />
   </div>
 </template>
+
+<style>
+  .preloader {
+    position: absolute;
+    left: calc(50% - 25px);
+
+    margin: 25px;
+  }
+</style>
--- src/stores/cars.js
+++ src/stores/cars.js
@@ -4,17 +4,33 @@ import { ref } from "vue";
 export const useCarsStore = defineStore('cars', () => {
   const carsData = ref([]);
   const isCarsLoaded = ref(false);
+  const isCarsPartLoaded = ref(false);
+  const carsFilesCount = 5;
+  let loadedFilesCount = 0;
 
   function fetchCars() {
-    import('../../data/cars-1.json')
+    if (loadedFilesCount < carsFilesCount) {
+      isCarsPartLoaded.value = false;
+      import(`../../data/cars-${loadedFilesCount + 1}.json`)
+        .then((data) => {
+          carsData.value.push(...data.default);
+          isCarsLoaded.value = true;
+          isCarsPartLoaded.value = true;
+          loadedFilesCount += 1;
+        })
+        .catch(() => {
+          isCarsLoaded.value = false;
+          isCarsPartLoaded.value = false;
+        });
+    }
+  }
+
+  function fetchNewData() {
+    import(`../../data/cars-2.json`)
       .then((data) => {
         carsData.value.push(...data.default);
-        isCarsLoaded.value = true;
-      })
-      .catch(() => {
-        isCarsLoaded.value = false;
       });
   }
 
-  return { carsData, fetchCars, isCarsLoaded };
+  return { carsData, fetchCars, isCarsLoaded, fetchNewData, isCarsPartLoaded };
 });
